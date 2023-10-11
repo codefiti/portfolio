3c57161508d556bb8c38dc3f258ea90197c7f8a5
Steve Tom <tomsteve187@gmail.com>
2023-10-11 13:50:14 +0300

    Configured Routes and simple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import './App.css'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
+
+// Your components
+import Home from './components/Home';
+import About from './components/AboutUs';
+import ContactUs from "./components/ContactUs.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
-  return (
-    <>
-    </>
-  )
+    return (
+        <Router>
+            <div>
+                {/* Navigation */}
+                <nav>
+                    <Navbar bg="light" expand="lg" />
+                </nav>
+
+                <Routes>
+                    <Route path="/about" element={<About />} />
+                    <Route path="/dashboard" element={<ContactUs />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+
+export default App;
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+const AboutUs = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>We are a company that values our customers.</p>
+        </div>
+    );
+}
+
+export default AboutUs;
