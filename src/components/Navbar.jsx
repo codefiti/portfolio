6ee20fd8aa8f314126e708b064a377099cecf59b
Steve Tom <tomsteve187@gmail.com>
2023-10-11 15:17:43 +0300

    Added logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 const Navbar = () => {
 
     return(
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-neutral">
             <div className="navbar-start">
-
+                <a className="text-2xl font-semibold text-white" href="/">
+                    <img src="../../public/codefiti.png" alt="logo" className="w-8 h-8" />
+                </a>
             </div>
             <div className="navbar-center">
 
             </div>
             <div className="navbar-end">
-                <div className="flex gap-5">
+                <div className="flex text-white gap-5">
                     <a className="" href="/">Home</a>
                     <a className="" href="/projects">Projects</a>
                     <a className="" href="/contacts">Contacts</a>
