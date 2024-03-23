a813f915185b6837a35f66797616973ada31b7c5
raccoon254 <tomsteve187@gmail.com>
2024-03-23 18:48:33 +0300

    --styled contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,36 @@
-function Contact (){
-    return(
-    <>
-    <div className="contact-container bg-mytheme-accent">
-                <h1 className="text-mytheme-primary">Get in Touch</h1>
+import "../contact.css";
+function Contact() {
+    return (
+        <>
+            <div className="main-container bg-mytheme-accent">
+                <h1 className="my-title">Get in Touch</h1>
                 <div className="contact-container">
                     <div className="contact-form-box">
-                        <form>
+                        <form className={'flex flex-col'}>
                             <div className="form-group">
                                 <label htmlFor="name" className="text-mytheme-primary">Name:</label>
-                                <input type="text" id="name" placeholder="Your full name" name="name" required className="input input-bordered" />
+                                <input type="text" id="name" placeholder="Your full name" name="name" required
+                                       className="input input-bordered"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="email" className="text-mytheme-primary">Email:</label>
-                                <input type="email" id="email" placeholder="Your email" name="email" required className="input input-bordered" />
+                                <input type="email" id="email" placeholder="Your email" name="email" required
+                                       className="input input-bordered"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="message" className="text-mytheme-primary">Message:</label>
-                                <textarea id="message" name="message" placeholder="Type your message here" rows="4" required className="textarea textarea-bordered"></textarea>
+                                <textarea id="message" name="message" placeholder="Type your message here" rows="4"
+                                          required className="textarea textarea-bordered"></textarea>
                             </div>
-                            <button type="submit" className="btn btn-primary">Send</button>
+                            <div className="btn-container w-full">
+                                <input type="submit" className="btn btn-primary bg-gray-50 w-full" value="Send"/>
+                            </div>
                         </form>
                     </div>
                 </div>
             </div>
-    </>
+        </>
     );
 }
+
 export default Contact;
